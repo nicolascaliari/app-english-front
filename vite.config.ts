12b81c31982566cd58a1e597363fe7b733f3efa1
barfer-dev <barferalimentos@gmail.com>
2026-08-26 01:36:52 +0200

    pwa

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
+      injectRegister: 'inline',
       includeAssets: ['favicon.svg'],
       manifest: {
         name: 'English Flashcards',
